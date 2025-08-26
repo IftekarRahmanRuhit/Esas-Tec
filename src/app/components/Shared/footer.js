459d75b57faaf7f5bca9382ec9335bfa3e7f028c
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-26 22:49:45 +0600

    updated footer

--- src/app/components/Shared/footer.js
+++ src/app/components/Shared/footer.js
@@ -79,7 +79,7 @@ export default function Footer() {
       <div className="relative z-10">
         {/* Main Footer Content */}
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-8">
             {/* Company Info */}
             <motion.div 
               className="lg:col-span-2"
